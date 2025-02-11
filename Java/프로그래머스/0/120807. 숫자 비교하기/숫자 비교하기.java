0a39339ac5ac53c7762faa6d8b5e90e0affb1b8e
박지영 <s2325@e-mirim.hs.kr>
2025-02-11 19:45:29 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        if(num1==num2){
+            return 1;
+        }else{
+            return -1; //answer 변수를 쓸 일 없이
+        }
+    }
+}
