1fbc0acbbbbd6e3ab91c1e32e7a2418a44ad8081
박지영 <s2325@e-mirim.hs.kr>
2025-02-21 12:59:59 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.05 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long[] solution(int x, int n) {
+        //배열 초기화
+        long[] answer = new long[n];
+        
+        int count = 0;
+        long repetition = x; //반복되는 값 (int로 하면 오버플로우 발생할수도 있기 때문에)
+        
+        for(int i = 0; i<n; i++){
+            answer[i] = repetition;
+            repetition += x;
+            count++;
+        }
+        return answer;
+    }
+}
