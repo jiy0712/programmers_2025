c03314953ae95eb98d8918b8a03242e89c3083ca
박지영 <s2325@e-mirim.hs.kr>
2025-02-23 16:48:48 +0900

    x만큼 간격이 있는 n개의 숫자.java

--- Java/프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
+++ Java/프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
@@ -4,7 +4,7 @@ class Solution {
         long[] answer = new long[n];
         
         int count = 0;
-        long repetition = x; //반복되는 값 (int로 하면 오버플로우 발생할수도 있기 때문에)
+        long repetition = x; // 반복되는 값 (int로 하면 오버플로우 발생할수도 있기 때문에)
         
         for(int i = 0; i<n; i++){
             answer[i] = repetition;
