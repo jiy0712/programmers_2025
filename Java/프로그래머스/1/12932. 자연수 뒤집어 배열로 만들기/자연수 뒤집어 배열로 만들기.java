4b3d269245f99c5a7b6d17d5f62d027310a2e75b
박지영 <s2325@e-mirim.hs.kr>
2025-02-26 21:57:09 +0900

    자연수 뒤집어 배열로 만들기.java

--- Java/프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ Java/프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -2,9 +2,9 @@ class Solution {
     public int[] solution(long n) {
         String nn = Long.toString(n); //long -> string으로 변환
         String[] n_string = nn.split(""); //한글자씩 잘라 string 배열에 넣기
-        int[] answer = new int[n_string.length]; //answer 방을 n_string만큼 생성해두기
+        int[] answer = new int[n_string.length]; //answer 방을 n_string만큼 생성
         for(int i=n_string.length-1; i>=0; i--){
-            //맨뒷자리 값부터 가져와 맨앞에 넣으며 채우기
+            //맨뒷자리 값부터 가져와 맨앞에 넣으며 채움
             answer[n_string.length-1-i] = Integer.parseInt(n_string[i]);
         }
         return answer;
